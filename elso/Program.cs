d33e8646ca1190805f661c3b4006b74d9d977b28
Pistipisti94 <pistipisti57@gmail.com>
2023-02-09 11:09:24 +0100

    igen

--- elso/Program.cs
+++ elso/Program.cs
@@ -14,31 +14,31 @@ namespace elso
             int szam = Convert.ToInt32(Console.ReadLine());
             if (szam % 3 == 0 && szam % 4 == 0 && szam % 9 == 0) 
             {
-                Console.WriteLine("A szám osztható 3-mal és 4-el és 9-el is");
+                Console.WriteLine("A szám osztható 3-mal és 4-el és 9-el is.");
             }
             else if (szam % 3 == 0 && szam % 9 == 0 ) 
             {
-                Console.WriteLine("A szám osztható 3-mal és 9-el");
+                Console.WriteLine("A szám osztható 3-mal és 9-el.");
             } 
             else if (szam % 3 == 0 && szam % 4 == 0 ) 
             {
-                Console.WriteLine("A szám osztható 3-mal és 4-el");
+                Console.WriteLine("A szám osztható 3-mal és 4-el.");
             }
             else if (szam % 3 == 0) 
             {
-                Console.WriteLine("A szám osztható 3-mal");
+                Console.WriteLine("A szám osztható 3-mal.");
             }
-            else if (szam%4==0)
+            else if (szam % 4 == 0)
             {
-                Console.WriteLine("A szám osztható 4-el");
+                Console.WriteLine("A szám osztható 4-el.");
             }
-            else if (szam%9==0)
+            else if (szam % 9 == 0)
             {
-                Console.WriteLine("A szám osztható 9-el");
+                Console.WriteLine("A szám osztható 9-el.");
             }
             else
             {
-                Console.WriteLine("Nem osztahtó");
+                Console.WriteLine("A szám nem osztható se 3-al se 4-el se 9-el.");
             }
             Console.ReadKey();
         }
---
+++ masodik/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masodik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg a hőmérsékletet: ");
+            int fok = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Celsiusból Fahrenheitbe, vagy Fahrenheitből Celsiusba váltsam?(1/2)");
+            int valasz = Convert.ToInt32(Console.ReadLine());
+            if (valasz == 1)
+            {
+                Console.WriteLine("A hőmérséklet " + (fok * 9/5 + 32) + "°C");
+            }
+            else if (valasz == 2)
+            {
+                Console.WriteLine("A hőmérséklet "+((fok-32)*5/9) + "°F");
+            }
+           Console.ReadKey();
+        }
+    }
+}
