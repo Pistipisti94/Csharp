bc6cf68bbbfbf63344689abf1f61d9f30f5473f7
Pistipisti94 <pistipisti57@gmail.com>
2023-02-09 10:34:37 +0100

    kesz

--- elso/Program.cs
+++ elso/Program.cs
@@ -10,6 +10,37 @@ namespace elso
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Szám:");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (szam % 3 == 0 && szam % 4 == 0 && szam % 9 == 0) 
+            {
+                Console.WriteLine("A szám osztható 3-mal és 4-el és 9-el is");
+            }
+            else if (szam % 3 == 0 && szam % 9 == 0 ) 
+            {
+                Console.WriteLine("A szám osztható 3-mal és 9-el");
+            } 
+            else if (szam % 3 == 0 && szam % 4 == 0 ) 
+            {
+                Console.WriteLine("A szám osztható 3-mal és 4-el");
+            }
+            else if (szam % 3 == 0) 
+            {
+                Console.WriteLine("A szám osztható 3-mal");
+            }
+            else if (szam%4==0)
+            {
+                Console.WriteLine("A szám osztható 4-el");
+            }
+            else if (szam%9==0)
+            {
+                Console.WriteLine("A szám osztható 9-el");
+            }
+            else
+            {
+                Console.WriteLine("Nem osztahtó");
+            }
+            Console.ReadKey();
         }
     }
 }
