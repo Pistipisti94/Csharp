4e69bc91e95ea89eb0a4661e3285463180b169c6
Pistipisti94 <pistipisti57@gmail.com>
2023-03-20 10:49:22 +0100

    folytkov

---
+++ Learn/CimletezesOOP/CimletezOOP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CimletezesOOP
+{
+    internal class CimletezOOP
+    {
+        private int a;
+        private int[] lista = {5,10,20,50,100,200,500,1000,2000,5000,10000,20000 };
+
+        public CimletezOOP() { }
+        public CimletezOOP(int a)
+        {
+            this.a = a;
+        }
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+        public int GetA() { return this.a; }
+        public void szamitasok(int a, int[] lista) 
+        {
+            int ennyi = 0;
+            int maradek;
+            for (int i = 0; i < lista.Length; i++)
+            {
+                if (a % lista[i] == 0)
+                {
+                    a= a / lista[i];
+                    ennyi++;
+                    maradek = a % lista[11]; 
+                }
+            }
+        }
+
+    }
+}
---
+++ Learn/CimletezesOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CimletezesOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pénz: ");
+            int penzek = Convert.ToInt32(Console.ReadLine());
+            CimletezOOP szamol= new CimletezOOP();
+            szamol.setA(penzek);
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
