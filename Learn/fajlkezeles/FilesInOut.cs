f853d614b773ed06e35fd8a240ddc41980239821
Pistipisti94 <pistipisti57@gmail.com>
2023-04-20 16:49:36 +0200

    Házi

---
+++ Learn/fajlkezeles/FilesInOut.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesIO
+{
+    internal class FileInpOut
+    {
+        // Osztályváltozók
+        private string fileName;
+        private FileMode fileMode;
+        private FileAccess fileAccess;
+        private FileStream fs;
+        private StreamReader sr;
+
+        // Konstruktor
+        public FileInpOut()
+        {
+            setFileName();  // Fájl nevének bekérése
+            setFileMode();  // Megnyitás módjának bekérése
+            setFileAccess();    // Hozzáférés módjának beállítása
+        }
+
+        private void setFileName()
+        {
+            Console.WriteLine("Adja meg a fájl nevét kiterjesztés nélkül:");
+            this.fileName = "..\\..\\" + Console.ReadLine() + ".txt";
+        }
+
+        private void setFileMode()
+        {
+            bool ismet;
+            string s;
+
+            Console.WriteLine("Adja meg a műveletet:");
+            Console.WriteLine("     cr:  Create");
+            // Létrehoz egy új fájlt, ha már létezik, a tartalmát kitörli.
+            Console.WriteLine("     crn: CreateNew");
+            // Mint előbb, de ha már létezik a fájl, akkor kivételt dob.
+            Console.WriteLine("     o:   Open");
+            // Megnyit egy fájlt, ha nem létezik, kivételt dob.
+            Console.WriteLine("     oc:  OpenOrCreate");
+            // Mint előbb, de ha nem létezik, akkor létrehozza a fájlt.
+            Console.WriteLine("     a:   Append");
+            // Megnyit egy fájlt és automatikusan a végére áll. Ha nem létezik, létrehozza.
+            Console.WriteLine("     t:   Truncate");
+            // Megnyit egy létező fájlt és törli a tartalmát.  Ebben a módban a fájl  tartalmát nem lehet olvasni (egyébként kivételt dob).
+
+            do
+            {
+                ismet = false;
+                s = Console.ReadLine().ToLower().Trim();
+                switch (s)
+                {
+                    case "cr": fileMode = FileMode.Create; break;
+                    case "crn": fileMode = FileMode.CreateNew; break;
+                    case "o": fileMode = FileMode.Open; break;
+                    case "oc": fileMode = FileMode.OpenOrCreate; break;
+                    case "a": fileMode = FileMode.Append; break;
+                    case "t": fileMode = FileMode.Truncate; break;
+                    default:
+                        Console.WriteLine("Hibás adat! Ismételje meg!");
+                        ismet = true;
+                        break;
+                }
+            } while (ismet);
+        }
+
+        private void setFileAccess()
+        {
+            bool ismet;
+            string s;
+
+            Console.WriteLine("Adja meg a fájl hozzáférést:");
+            Console.WriteLine("     r:  Olvasás");
+            Console.WriteLine("     w:  Írás");
+            Console.WriteLine("     rw: Olvasás és írás");
+
+            do
+            {
+                ismet = false;
+                s = Console.ReadLine().ToLower().Trim();
+                switch (s)
+                {
+                    case "r": fileAccess = FileAccess.Read; break;
+                    case "w": fileAccess = FileAccess.Write; break;
+                    case "rw": fileAccess = FileAccess.ReadWrite; break;
+                    default:
+                        Console.WriteLine("Hibás adat! Ismételje meg!");
+                        ismet = true;
+                        break;
+                }
+            } while (ismet);
+        }
+
+        public FileStream getFile()
+        {
+            return new FileStream(this.fileName, this.fileMode, this.fileAccess);
+        }
+
+        public void closeFile()
+        {
+            if (this.sr != null) this.sr.Close();
+            if (this.fs != null) this.fs.Close();
+        }
+
+    }
+}
---
+++ Learn/fajlkezeles/Program .cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesIO
+{
+    internal class Program
+    {
+        #region Egész fájl olvasása
+
+        #endregion Egész fájl olvasása
+
+        static void Main(string[] args)
+        {
+            FileInpOut f = new FileInpOut();
+
+           // fileReadFull(f.getFile());
+
+            fileReadLine(f.getFile());
+            
+            string[] s = { };
+            fileWrite(f.getFile(), s);
+
+            f.closeFile();
+
+            Console.ReadKey();
+        }
+
+        #region File írása
+        private static void fileWrite(FileStream file, string[] s)
+        {
+            using(StreamWriter sr= new StreamWriter(file))
+            {
+                // Dolgozunk a fájllal
+                Console.WriteLine("Írja a fájlt");
+                // Soronként olvas
+                for (int i = 0; i < s.Length; i++)
+                {
+                    sr.WriteLine(s[i]);
+                    Console.WriteLine(s); //Kiírás ellenőrzése
+                }
+            }
+        }
+        #endregion File írás
+
+        #region Soronkénti olvasás
+        private static void fileReadLine(FileStream file)
+        {
+            using (StreamReader sr = new StreamReader(file))
+            {
+                string[] s; 
+                // Dolgozunk a fájllal
+                Console.WriteLine("Soronként olvassa a fájlt");
+                // Soronként olvas
+                for (int i = 0; i < 20; i++)
+                {
+                    string s = sr.ReadLine();
+                    s.Add
+                    Console.WriteLine(s);
+                }
+                
+
+                
+
+            }
+        }
+        #endregion Soronkénti olvasás
+
+    }
+}
