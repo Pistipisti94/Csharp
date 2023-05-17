5a2e40780b8dcd5845a9d1619bc8965bbc0a1d3e
pistipisti94 <pistipisti57@gmail.com>
2023-05-17 09:51:51 +0200

    folykov

---
+++ Learn/BaratokOOP2/Baratok.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BaratokOOP2
+{
+    public struct Szemely
+    {
+        public string nev;
+        public DateTime szido;
+        public char nem;
+        public int hajlam;
+    }
+    internal class Baratok
+    {
+        private static string path = "..\\..\\";
+        private static string inpfile = "lista.csv";
+        private static string outfile = "barat.csv";
+        private List<Szemely> list = new List<Szemely>();
+        #region Konstruktor
+        public Baratok() { }
+        #endregion Konstruktor
+
+        #region Metódusok
+        #region Beolvasás
+        public void beolvas() 
+        {
+            FileStream fs = new FileStream(path + inpfile,FileMode.Open,FileAccess.Read);
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                while (!sr.EndOfStream)
+                    this.list.Add(convert(sr.ReadLine().Split(';')));
+            }
+        }
+        #endregion Beolvasás
+        #region Személy
+        private Szemely convert(string[] line) 
+        {
+            Szemely szemely = new Szemely();
+            szemely.nev = line[0];
+            szemely.szido = Convert.ToDateTime(line[1]);
+            szemely.nem = Convert.ToChar(line[2]);
+            szemely.hajlam = Convert.ToInt32(line[3]);
+
+            return szemely;
+        }
+        #endregion Személy
+        #region Hozzáadás
+        public void insertBarat(string[] t) 
+        {
+            this.list.Add(convert(t));
+        }
+        #endregion Hozzáadás
+        #region Törlés
+        public void delete(string nev)
+        {
+            this.list.Remove(this.list.Find(list => list.nev == nev));
+        }
+        #endregion Törlés
+        #region Keresés
+        public bool kereses(string nev)
+        {
+            Szemely vane = new Szemely();
+            vane = this.list.Find(list => list.nev == nev);
+            if (vane.nev == nev)
+                return true;
+            return false;
+        }
+        #endregion Keresés
+        #region Kiiratás
+        public void kiir()
+        {
+            using (StreamWriter sw = new StreamWriter(new FileStream(path + outfile, FileMode.Create, FileAccess.Write), Encoding.UTF8))
+            {
+                foreach (Szemely item in this.list)
+                    sw.WriteLine(item.nev,";",item.szido, ";", item.nem, ";", item.hajlam);
+            }
+        }
+        #endregion Kiiratás
+        #region Kepreiras
+        public void odair()
+        {
+            FileStream fs = new FileStream(path + inpfile, FileMode.Open, FileAccess.Read);
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                while (!sr.EndOfStream)
+                    
+            }
+        }
+        #endregion Kepreiras
+        #endregion Metódusok
+
+
+    }
+}
---
+++ Learn/BaratokOOP2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratokOOP2
+{
+    internal class Program
+    {
+        private static Baratok baratok;
+
+        static void Main(string[] args)
+        {
+            baratok = new Baratok();
+            string[] t = { "teszt aranka", "1991.11.12", "N", "4" };
+
+            baratok.beolvas();
+            vizsga(t[0]);
+
+            baratok.insertBarat(t);
+            vizsga(t[0]);
+
+            baratok.delete(t[0]);
+            vizsga(t[0]);
+
+            
+
+            Console.ReadKey();
+        }
+        private static void vizsga(string t)
+        {
+            if (baratok.kereses(t))
+                Console.WriteLine("Már van ilyen barátunk.");
+            else
+                Console.WriteLine("Nincs ilyen barátunk.");
+        }
+    }
+}
